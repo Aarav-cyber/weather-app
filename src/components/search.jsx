029a76e1eb824cf351767a11d502fc14559ba014
Aarav Raj Shrestha <aravraj331@gmial.com>
2025-08-11 18:53:00 +0530

    Changes on search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,11 +1,20 @@
 import { AsyncPaginate } from 'react-select-async-paginate';
+import { useState } from 'react';
 
 export default function Search() {
+    const [search, setSearch] = useState(null);
+    
+    const handleChange = (newValue) => {
+        setSearch(newValue)
+    }
+
+
     return (
         <AsyncPaginate
             placeholder="Search for a city..."
             debounceTimeout={600}
-            value= {search}
+            value={search}
+            onChange={handleChange}
         />
     );
 }
