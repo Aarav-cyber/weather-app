c78fcd3efbfe5b72a518b80dc8cb7afebfa6a020
Aarav Raj Shrestha <aravraj331@gmial.com>
2025-08-11 23:21:01 +0530

    Added api

---
+++ src/components/api.js
@@ -0,0 +1,11 @@
+export const Url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const Options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'efb7bfe899msh34eea08018ce46cp1f14dajsn89e29b842c19',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,20 +1,38 @@
-import { AsyncPaginate } from 'react-select-async-paginate';
-import { useState } from 'react';
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { Url, Options } from "./api.js";
 
-export default function Search() {
-    const [search, setSearch] = useState(null);
-    
-    const handleChange = (newValue) => {
-        setSearch(newValue)
+export default function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+
+  const handleChange = (newValue) => {
+    setSearch(newValue);
+    onSearchChange(newValue);
+  };
+
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(`${Url}?q=${inputValue}`, Options);
+      const result = await response.json();
+      return {
+        options: result.data.map((city) => ({
+          label: `${city.name}, ${city.country}`,
+          value: city.id,
+        })),
+      };
+    } catch (error) {
+      console.error(error);
+      return { options: [] };
     }
+  };
 
-
-    return (
-        <AsyncPaginate
-            placeholder="Search for a city..."
-            debounceTimeout={600}
-            value={search}
-            onChange={handleChange}
-        />
-    );
+  return (
+    <AsyncPaginate
+      placeholder="Search for a city..."
+      debounceTimeout={600}
+      value={search}
+      onChange={handleChange}
+      loadOptions={loadOptions}
+    />
+  );
 }
